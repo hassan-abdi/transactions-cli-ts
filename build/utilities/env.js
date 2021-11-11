5fc563d28ecf8d0fee47fa81cf7f3c759becb490
hassan.abdi@outlook.com <newDellC500GitLab>
2021-11-10 23:25:34 -0800

    Removing ignored files

--- build/utilities/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-function env(key, defaultValue) {
-    return (process.env[key]) ?? defaultValue;
-}
-exports.env = env;
